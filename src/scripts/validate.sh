4194e0c847a5686135d2753f0dcf36dae246143e
amitrezilion <amits@rezilion.com>
2022-12-05 16:38:23 +0200

    Preparing for v2.4.0

--- src/scripts/validate.sh
+++ src/scripts/validate.sh
@@ -12,16 +12,10 @@ mkdir -p $ARTIFACT_PATH
 for dir in "$REZILION_OUTPUTS_FOLDER"/*/
   do
     ENCODED_DIR=$(basename -- "${dir%*/}")
-
-    if [[ $REZILION_IMAGE_TO_SCAN == "0" ]] ; then
-      IMAGE_NAME_DECODED=$(echo -n "$ENCODED_DIR" | base64 --decode) || true
-    else
-      IMAGE_NAME_DECODED=$REZILION_IMAGE_TO_SCAN
-    fi
+    IMAGE_NAME_DECODED=$(echo -n "$ENCODED_DIR" | base64 --decode)
 
     NORMALIZED_IMAGE_NAME=$(basename -- "$IMAGE_NAME_DECODED")
     VALIDATE_COMMAND="$REZILION_AGENT_PATH --log-path $REZILION_OUTPUTS_FOLDER/$ENCODED_DIR/validate_$NORMALIZED_IMAGE_NAME.log --license-key $REZILION_LICENSE_KEY --ci-environment circleci validate --tests-directory $REZILION_OUTPUTS_FOLDER/$ENCODED_DIR --html-report-path $ARTIFACT_PATH/report_$NORMALIZED_IMAGE_NAME.html --json-report-path $ARTIFACT_PATH/report_$NORMALIZED_IMAGE_NAME.json --cyclonedx-report-path $ARTIFACT_PATH/cyclonedx_report_$NORMALIZED_IMAGE_NAME.json --container-image-name $IMAGE_NAME_DECODED --scanner-name trivy"
-    echo "$VALIDATE_COMMAND"
 
     if [[ -n "$REZILION_DONT_SAVE_TEST_LOGS" ]]; then
       $VALIDATE_COMMAND &> /dev/null || true
