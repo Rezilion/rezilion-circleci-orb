6c6a6bdaa6011d543ffb53202f08ba150b691051
amitrezilion <amits@rezilion.com>
2022-12-06 15:33:26 +0200

    Preparing for v2.4.1

--- src/scripts/gates.sh
+++ src/scripts/gates.sh
@@ -15,13 +15,7 @@ ARTIFACT_PATH=/tmp/rezilion/artifacts
 for dir in "$REZILION_OUTPUTS_FOLDER"/*/
   do
     ENCODED_DIR=$(basename -- "${dir%*/}")
-
-    if [[ $REZILION_IMAGE_TO_SCAN == "0" ]] ; then
-      IMAGE_NAME_DECODED=$(echo -n "$ENCODED_DIR" | base64 --decode)
-    else
-      IMAGE_NAME_DECODED=$REZILION_IMAGE_TO_SCAN
-    fi
-
+    IMAGE_NAME_DECODED=$(echo -n "$ENCODED_DIR" | base64 --decode)
     NORMALIZED_IMAGE_NAME=$(basename -- "$IMAGE_NAME_DECODED")
     $REZILION_AGENT_PATH --license-key "$REZILION_LICENSE_KEY" --ci-environment circleci gates --severity-gate "$REZILION_SEVERITY_GATE" --json-report-path $ARTIFACT_PATH/report_"$NORMALIZED_IMAGE_NAME".json
 
