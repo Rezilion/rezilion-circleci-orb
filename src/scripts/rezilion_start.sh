bfc4865efc0396866f9f42b4eb268294f04b57e5
amitrezilion <amits@rezilion.com>
2023-03-19 17:17:20 +0200

    Update v2.8.0

--- src/scripts/rezilion_start.sh
+++ src/scripts/rezilion_start.sh
@@ -1,7 +1,7 @@
 REZILION_OUTPUTS_FOLDER=/tmp/rezilion/outputs
 REZILION_AGENT_PATH=/tmp/rezilion/validate_ci_agent
 
-BASE64_IMAGE="$(echo -n "$REZILION_IMAGE_TO_SCAN" | base64)"
+BASE64_IMAGE="$(echo -n "$REZILION_IMAGE_TO_SCAN" | base64 -w 0)"
 REZILION_JOB_FOLDER=$REZILION_OUTPUTS_FOLDER/$BASE64_IMAGE/$CIRCLE_JOB
 
 if [[ $CIRCLE_NODE_TOTAL -gt 1 ]] ; then
@@ -11,6 +11,7 @@ mkdir -p "$REZILION_JOB_FOLDER"
 
 REZILION_LDPRELOAD_OUTPUT_DIRECTORY=$REZILION_JOB_FOLDER/ldpreload
 MINERS_STATUS_FILE_PATH=$REZILION_JOB_FOLDER/miners_status
+MINERS_LOG_PATH="$REZILION_JOB_FOLDER/$(basename -- "$REZILION_IMAGE_TO_SCAN")_miners.log"
 
 echo "$REZILION_USER_COMMAND"
 
@@ -18,7 +19,7 @@ if [ -f /tmp/rezilion/stop_mining ]; then
   rm /tmp/rezilion/stop_mining
 fi
 
-$REZILION_AGENT_PATH --license-key "$REZILION_LICENSE_KEY" --ci-environment circleci miners --db-path "$REZILION_JOB_FOLDER/db" --ld-preload-output-directory "$REZILION_LDPRELOAD_OUTPUT_DIRECTORY" --miners-status-file "$MINERS_STATUS_FILE_PATH" --stop-dynamic-mining-flag-path /tmp/rezilion/stop_mining --shell-command-to-mine "$REZILION_USER_COMMAND" --shell-command-output-file-path /tmp/rezilion/shell_command_output --shell-command-exit-code-file-path /tmp/rezilion/shell_command_exit_code >> "$REZILION_JOB_FOLDER/miners.log" 2>&1 || REZILION_FAILED_RUNNING=1
+$REZILION_AGENT_PATH --license-key "$REZILION_LICENSE_KEY" --ci-environment circleci miners --db-path "$REZILION_JOB_FOLDER/db" --ld-preload-output-directory "$REZILION_LDPRELOAD_OUTPUT_DIRECTORY" --miners-status-file "$MINERS_STATUS_FILE_PATH" --stop-dynamic-mining-flag-path /tmp/rezilion/stop_mining --shell-command-to-mine "$REZILION_USER_COMMAND" --shell-command-output-file-path /tmp/rezilion/shell_command_output --shell-command-exit-code-file-path /tmp/rezilion/shell_command_exit_code >> "$MINERS_LOG_PATH" 2>&1 || REZILION_FAILED_RUNNING=1
 
 if [ -z "$REZILION_FAILED_RUNNING" ]; then
   cat /tmp/rezilion/shell_command_output
--- src/scripts/validate.sh
+++ src/scripts/validate.sh
@@ -1,7 +1,7 @@
-REZILION_OUTPUTS_FOLDER=/tmp/rezilion/outputs
+REZILION_MINERS_OUTPUT_FOLDER=/tmp/rezilion/outputs
 REZILION_AGENT_PATH=/tmp/rezilion/validate_ci_agent
 
-if [[ -z "$(ls -A $REZILION_OUTPUTS_FOLDER)" ]]; then
+if [[ -z "$(ls -A $REZILION_MINERS_OUTPUT_FOLDER)" ]]; then
   echo "Rezilion found no images to validate..."
   exit 0
 fi
@@ -9,13 +9,19 @@ fi
 ARTIFACT_PATH=/tmp/rezilion/artifacts
 mkdir -p $ARTIFACT_PATH
 
-for dir in "$REZILION_OUTPUTS_FOLDER"/*/
+if [[ -z "$REZILION_DONT_SAVE_TEST_LOGS" ]]; then
+  tar cfz $ARTIFACT_PATH/logs.tar.gz $REZILION_MINERS_OUTPUT_FOLDER || true
+fi
+
+export REZILION_BACKEND_API_GATEWAY_URL="https://api.rezilion.com"
+
+for dir in "$REZILION_MINERS_OUTPUT_FOLDER"/*/
   do
     ENCODED_DIR="$(basename -- "${dir%*/}")"
     IMAGE_NAME_DECODED=$(echo -n "$ENCODED_DIR" | base64 --decode)
     NORMALIZED_IMAGE_NAME="$(basename -- "$IMAGE_NAME_DECODED")"
-    
-    VALIDATE_COMMAND="$REZILION_AGENT_PATH --log-path $REZILION_OUTPUTS_FOLDER/$ENCODED_DIR/validate_$NORMALIZED_IMAGE_NAME.log --license-key $REZILION_LICENSE_KEY --ci-environment circleci validate --tests-directory $REZILION_OUTPUTS_FOLDER/$ENCODED_DIR --html-report-path $ARTIFACT_PATH/report_$NORMALIZED_IMAGE_NAME.html --json-report-path $ARTIFACT_PATH/report_$NORMALIZED_IMAGE_NAME.json --cyclonedx-report-path $ARTIFACT_PATH/cyclonedx_report_$NORMALIZED_IMAGE_NAME.json --container-image-name $IMAGE_NAME_DECODED --scanner-name trivy"
+
+    VALIDATE_COMMAND="$REZILION_AGENT_PATH --log-path "$REZILION_MINERS_OUTPUT_FOLDER/$ENCODED_DIR/$NORMALIZED_IMAGE_NAME"_validate.log --license-key $REZILION_LICENSE_KEY --ci-environment circleci validate --tests-directory $REZILION_MINERS_OUTPUT_FOLDER/$ENCODED_DIR --html-report-path $ARTIFACT_PATH/report_$NORMALIZED_IMAGE_NAME.html --json-report-path $ARTIFACT_PATH/report_$NORMALIZED_IMAGE_NAME.json --cyclonedx-report-path $ARTIFACT_PATH/cyclonedx_report_$NORMALIZED_IMAGE_NAME.json --container-image-name $IMAGE_NAME_DECODED --scanner-name Rezilion"
 
     if [[ -n "$REZILION_DONT_SAVE_TEST_LOGS" ]]; then
       $VALIDATE_COMMAND &> /dev/null || true
@@ -26,5 +32,5 @@ for dir in "$REZILION_OUTPUTS_FOLDER"/*/
   done
 
 if [[ -z "$REZILION_DONT_SAVE_TEST_LOGS" ]]; then
-  tar cfz $ARTIFACT_PATH/logs.tar.gz $REZILION_OUTPUTS_FOLDER || true
+  tar cfz $ARTIFACT_PATH/logs.tar.gz $REZILION_MINERS_OUTPUT_FOLDER || true
 fi
