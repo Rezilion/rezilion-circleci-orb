7b701c52b3ca254e0812d9c53e195298f92174d1
amitrezilion <instructor.ci@rezilion.com>
2023-05-04 10:24:03 +0300

    Preparing for v2.11.0

--- src/scripts/validate.sh
+++ src/scripts/validate.sh
@@ -16,21 +16,13 @@ fi
 export REZILION_BACKEND_API_GATEWAY_URL="https://api.rezilion.com"
 export REZILION_BACKEND_API_EUROPE_GATEWAY_URL="https://api.eu.rezilion.com"
 
-for dir in "$REZILION_MINERS_OUTPUT_FOLDER"/*/
-  do
-    ENCODED_DIR="$(basename -- "${dir%*/}")"
-    IMAGE_NAME_DECODED=$(echo -n "$ENCODED_DIR" | base64 --decode)
-    NORMALIZED_IMAGE_NAME="$(basename -- "$IMAGE_NAME_DECODED")"
+VALIDATE_COMMAND="$REZILION_AGENT_PATH --log-path $REZILION_MINERS_OUTPUT_FOLDER/validate.log --license-key $REZILION_LICENSE_KEY --ci-environment circleci validate --tests-directory $REZILION_MINERS_OUTPUT_FOLDER --output-directory $ARTIFACT_PATH --scanner-name Rezilion"
 
-    VALIDATE_COMMAND="$REZILION_AGENT_PATH --log-path "$REZILION_MINERS_OUTPUT_FOLDER/$ENCODED_DIR/$NORMALIZED_IMAGE_NAME"_validate.log --license-key $REZILION_LICENSE_KEY --ci-environment circleci validate --tests-directory $REZILION_MINERS_OUTPUT_FOLDER/$ENCODED_DIR --html-report-path $ARTIFACT_PATH/report_$NORMALIZED_IMAGE_NAME.html --json-report-path $ARTIFACT_PATH/report_$NORMALIZED_IMAGE_NAME.json --cyclonedx-report-path $ARTIFACT_PATH/cyclonedx_report_$NORMALIZED_IMAGE_NAME.json --container-image-name $IMAGE_NAME_DECODED --scanner-name Rezilion"
-
-    if [[ -n "$REZILION_DONT_SAVE_TEST_LOGS" ]]; then
-      $VALIDATE_COMMAND &> /dev/null || true
-    else
-      $VALIDATE_COMMAND || true
-    fi
-
-  done
+if [[ -n "$REZILION_DONT_SAVE_TEST_LOGS" ]]; then
+  $VALIDATE_COMMAND &> /dev/null
+else
+  $VALIDATE_COMMAND
+fi
 
 if [[ -z "$REZILION_DONT_SAVE_TEST_LOGS" ]]; then
   tar cfz $ARTIFACT_PATH/logs.tar.gz $REZILION_MINERS_OUTPUT_FOLDER || true
